b670d1481955fd62edf12d5766b167be0494873c
Takahiro Murakami <tayuusfka@gmail.com>
2023-04-24 05:49:11 +0900

    first initialized backend

---
+++ backend/knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development : {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+        directory: "./migrations"
+    },
+    seeds: {
+        directory: "./seed"
+    }
+  }
+
+
+};
