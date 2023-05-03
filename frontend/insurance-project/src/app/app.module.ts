250ed6d063d4670326bf8bf06381671a36380734
Takahiro Murakami <tayuusfka@gmail.com>
2023-05-03 11:19:21 +0900

    changed folder structure inside app folder.

--- frontend/insurance-project/src/app/app.module.ts
+++ frontend/insurance-project/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { ProductsComponent } from './products/products.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
-import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,11 +27,13 @@ import { FooterComponent } from './footer/footer.component';
     ProductsComponent,
     TestimonialsComponent,
     ContactComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
