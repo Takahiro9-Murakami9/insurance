8f857fdaa543c0150758a5034938d1cd37b1f722
Takahiro Murakami <tayuusfka@gmail.com>
2023-06-24 15:00:19 +0900

    feat:added slick carousel in the product component

--- frontend/insurance-project/src/app/app.module.ts
+++ frontend/insurance-project/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -34,6 +35,7 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/insurance-project/src/app/products/products.component.ts
+++ frontend/insurance-project/src/app/products/products.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.sass']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  slides = [
+    { image: 'assets/images/unchi_character.png' },
+    { image: 'assets/images/Dragon_sheep.jpg' },
+    { image: 'assets/images/stroberry_apple.jpg' },
+    { image: 'assets/images/20210227224909.png '}
+  ];
 
+  slideConfig = {
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
+
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
+  constructor() {}
+  ngOnInit(): void {}
 }
